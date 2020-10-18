fcce605c56644b0703f883de4b2f4bf8983e3293
J1a4b4 <46267154+J1a4b4@users.noreply.github.com>
2020-10-18 12:58:53 -0700

    LambdaButtons, LambdaMethods(IP)
    :smile:

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,15 +1,22 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
+	
 	private JFrame window = new JFrame();
-	private JButton addNumbers = new JButton("ADD 2 Numbers");
+	private JButton addNumbers = new JButton("ADD 2 NUMBERS");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
+	
+	Random rand;
 	
 	public LambdaButtons() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,16 +25,35 @@ public class LambdaButtons {
 		window.add(randNumber);
 		window.add(tellAJoke);
 		
-		//1. Call the addActionListener methods for each button. Use lambdas
-		//   to define to functionality of the buttons.
-		
+		rand = new Random();
+
+		// 1. Call the addActionListener methods for each button. Use lambdas
+		// to define to functionality of the buttons.
+		addNumbers.addActionListener((e) -> {
+			int i1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the first number."));
+			int i2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the second number."));
+			JOptionPane.showMessageDialog(null, i1+i2);
+			});
+		randNumber.addActionListener((e) -> {
+			JOptionPane.showMessageDialog(null, rand.nextInt());
+		});
+		tellAJoke.addActionListener((e) -> {
+			String joke = "";
+			int i = rand.nextInt(3);
+			if (i == 0) {
+				joke = "Need joke: add in Post";
+			}else if (i == 1) {
+				joke = "People of varied ethnicities walk into a bar.";
+			}else {
+				joke = "You thought it was a joke, but it was me, Clichéd Reference!";
+			}
+			JOptionPane.showMessageDialog(null, joke);
+		});
 		window.setVisible(true);
 		window.pack();
 	}
-	
+
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
-	
-	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -3,7 +3,7 @@ package _01_Lambda_Methods;
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+		//Done.
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
 		printCustomMessage((s)->{
@@ -13,11 +13,44 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		SpecialPrinter backwards = (s) -> {
+			String s1 = "";
+			for (int i = 0; i < s.length(); i++) {
+				s1 += s.charAt(i);
+			}
+			s = "";
+			for (int i = s1.length() - 1; i >= 0; i--) {
+				s += s1.charAt(i);
+			}
+		};
+		printCustomMessage(backwards, "String");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		SpecialPrinter mixCase = (s) -> {
+			String s1 = "";
+			for (int i = 0; i < s.length(); i++) {
+				s1 += s.charAt(i);
+			}
+			s = "";
+			for (int i = 0; i < s1.length(); i++) {
+				if (i % 2 == 0) {
+					s += s1.substring(i, i + 1).toLowerCase();
+				}else if (i % 2 == 1) {
+					s += s1.substring(i, i + 1).toUpperCase();
+				}
+			}
+		};
+		printCustomMessage(mixCase, "String");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		SpecialPrinter periodPuntuated = (s) -> {
+			String s1 = "";
+			for (int i = 0; i < s.length(); i++) {
+				s1 += s.charAt(i);
+			}
+			s = "";
+			for (int i = 0; i < s1.length(); i++) {
+				s += s1.charAt(i) + ".";
+			}
+		};
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
 	
 	}
